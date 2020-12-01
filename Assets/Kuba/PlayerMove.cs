d64a7196b4ba07cb9500e49f4a4a2e6e17d52303
kuba <kubababec@gmail.com>
2020-12-01 11:36:07 +0100

    kolo

---
+++ Assets/Kuba/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	[SerializeField] float speed = 1;
+	bool isGrounded = true;
+	float horizontal;
+	float vertical;
+	Rigidbody2D rb;
+
+	Vector2 jump;
+	[SerializeField] float jumpForce = 1;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+		jump = new Vector2(0, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
+
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(new Vector2(horizontal * speed * Time.fixedDeltaTime, 0));
+		}
+
+	}
+
+	
+}
---
+++ Assets/Kuba/RadialMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialMenu : MonoBehaviour
+{
+	[SerializeField] GameObject theMenu;
+	public Vector2 moveInput;
+	int selectedOption;
+	[SerializeField] GameObject selector;
+
+	bool isActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			isActive = !isActive;
+			theMenu.SetActive(isActive);
+		}
+
+		if (theMenu.activeInHierarchy)
+		{
+			moveInput.x = Input.mousePosition.x - (Screen.width / 2f);
+			moveInput.y = Input.mousePosition.y - (Screen.height / 2f);
+			moveInput.Normalize();
+
+			if(moveInput != Vector2.zero)
+			{
+				float angle = Mathf.Atan2(moveInput.y, -moveInput.x) / Mathf.PI;
+				angle *= 180;
+				if(angle < 0)
+				{
+					angle += 360;
+				}
+
+				for (int i = 0; i < 6; i++)
+				{
+					if(angle > i * 60 && angle < (i +1) * 60)
+					{
+						selectedOption = i + 1;
+						selector.transform.rotation = Quaternion.Euler(0, 0, i * -60);
+					}
+				}
+
+			}
+
+		}
+    }
+}
