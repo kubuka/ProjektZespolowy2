fdfb70e3ee7565c1970f1046dd4d4e90d6ecd762
kubuka <kubababec@gmail.com>
2020-12-16 13:48:26 +0100

    dialogue

---
+++ Assets/Kuba/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue d)
+    {
+        sentences.Clear();
+
+        nameText.text = d.name;
+
+        foreach (string sentence in d.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TyprSentence(sentence));
+    }
+
+    IEnumerator TyprSentence(string sentence)
+    {
+        dialogueText.text = "";
+
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+
+    }
+}
