3c236238c00a652fea5be20ffdab4bc8a3ff4d60
kubuka <kubababec@gmail.com>
2020-12-18 09:16:31 +0100

    backpack

---
+++ Assets/Kuba/Item.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public bool isTouchingPlayer = false;
+    Backpack bp;
+
+    private void Start()
+    {
+        bp = FindObjectOfType<Backpack>();
+    }
+
+    private void Update()
+    {
+        if (isTouchingPlayer && Input.GetKeyDown(KeyCode.Q))
+        {
+            bp.slots[bp.taken].sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+            bp.taken++;
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isTouchingPlayer = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isTouchingPlayer = false;
+    }
+}
