1d447031354f8f9997fb0e3175f49533c6fe70c9
Janek <jan.deregowski@onet.eu>
2021-02-03 14:13:33 +0100

    menu

---
+++ Assets/Scener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scener : MonoBehaviour
+{
+    public void StartAnim()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
